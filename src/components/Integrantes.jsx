02b342b94436bd9b4354542a7c86cfb329d6007b
Julia Souza <julia.souza18@eaportal.org>
2024-08-12 17:16:39 -0300

    foto no retangulo

--- src/components/Integrantes.jsx
+++ src/components/Integrantes.jsx
@@ -6,10 +6,13 @@ export default function Integrantes() {
       <section className='flex flex-col items-center justify-center p-6'>
         <h1 className='uppercase text-center mt-4 mb-7 font-bold text-xl  text-sobre'>Sobre nós</h1>
         <section>
-            <div className='w [345px] p-8 h-[488px]  justify-normal items-center bg-fundoHeader left-10 rounded-[8px]'>
-            <p className=''>Pedro H. Gomes</p>
-            <p className=''>Idade:17 anos</p>
-            <p className=''>Formação: Unasp- Técnico de Informática</p></div>
+            <div className='w [345px] p-8 h-[488px]  flex flex-col justify-normal items-center bg-fundoHeader left-10 rounded-[8px]'>
+              <img className=' w-40 h-40 ' src="/src/assets/judecosta 2.png" alt="" />
+            <p className=''>Julia Schiavi</p>
+            <p className=''>Idade:18 anos</p>
+            <p className=''>Formação: Unasp- Técnico de Informática</p>
+            <a href="https://www.instagram.com/juh_schiavi18?igsh=ZnVzMHZ1dWc2am4x&utm_source=qr"> <img src="/src/assets/insta.png" alt="" /></a>
+            </div>
             
         </section>
       </section>
