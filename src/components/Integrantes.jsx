dca5dadd5e4bb495eabbd085c853b3fafb319e32
Julia Souza <julia.souza18@eaportal.org>
2024-08-11 23:09:56 -0300

    primeira caixa

--- src/components/Integrantes.jsx
+++ src/components/Integrantes.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 export default function Integrantes() {
   return (
     <div>
-      <section className='flex flex-col items-center justify-cente p-6'>
-        <h1 className='uppercase text-center mt-4 mb-7 font-bold text-xl md:text-3xl text-sobre'>Sobre nós</h1>
+      <section className='flex flex-col items-center justify-center p-6'>
+        <h1 className='uppercase text-center mt-4 mb-7 font-bold text-xl  text-sobre'>Sobre nós</h1>
+        <section>
+            <div className='w [345px] p-8 h-[488px]  justify-normal items-center bg-fundoHeader left-10 rounded-[8px]'>
+            <p className=''>Pedro H. Gomes</p>
+            <p className=''>Idade:17 anos</p>
+            <p className=''>Formação: Unasp- Técnico de Informática</p></div>
+            
+        </section>
       </section>
     </div>
   )
