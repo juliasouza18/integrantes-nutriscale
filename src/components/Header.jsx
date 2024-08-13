3555f38f300f88dab9a2fbe21e9433045a03db41
Julia Souza <julia.souza18@eaportal.org>
2024-08-12 21:28:52 -0300

    implementei o menu hamburgúer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '/LOGO.svg'
 
 export default function Header() {
@@ -9,9 +9,9 @@ export default function Header() {
            <img src={Logo} alt="" className='w-12 h-8 md:w-20 md:h-12 mr-3 md:mr-auto'/>
         </button>
 
-        <nav className='flex-grow'>
+        <nav className='flex-grow hidden md:inline'>
             <ul className='flex md:justify-end space-x-4 md:space-x-6 text-xs md:text-base uppercase text-naoSelecionado font-bold'>
-                <li className='flex text-botaoSelecionado underline decoration-botaoSelecionado decoration-2 '>
+                <li className='flex'>
                     <a href="">Home</a>
                 </li>
                 <li className= 'whitespace-nowrap'>
@@ -20,14 +20,44 @@ export default function Header() {
                 <li>
                     <a href="">Materiais</a>
                 </li>
-                <li>
+                <li className=''>
                     <a href="">Referências</a>
                 </li>
-                <li className='whitespace-nowrap'>
+                <li className='whitespace-nowrap text-botaoSelecionado underline decoration-botaoSelecionado decoration-2'>
                     <a href="">Sobre nós</a>
                 </li>
             </ul>
         </nav>
+
+        {/*Menu Hamburgui kkkkkacabapeloamor*/}
+        <button className='space-y-1 group md:hidden'>
+            <div className='w-6 h-1 bg-white'></div>
+            <div className='w-6 h-1 bg-white'></div>
+            <div className='w-6 h-1 bg-white'></div>
+
+            {/* menu */}
+            <ul className='bg-fundoHeader w-screen pb-10 absolute -top-full group-focus:top-0 right-0 duration-150 flex flex-col space-y-3 justify-end text-naoSelecionado'>
+                <button className='px-10 py-8 relative ml-auto'>
+                    <div className='w-6 h-1 rotate-45 absolute bg-white'></div>
+                    <div className='w-6 h-1 -rotate-45 absolute bg-white'></div>
+                </button>
+                <li className='flex justify-center w-full '>
+                    <a href="">Home</a>
+                </li>
+                <li className= 'whitespace-nowrap'>
+                    <a href="">Como Usar</a>
+                </li>
+                <li>
+                    <a href="">Materiais</a>
+                </li>
+                <li className=''>
+                    <a href="">Referências</a>
+                </li>
+                <li className='whitespace-nowrap text-botaoSelecionado'>
+                    <a href="">Sobre nós</a>
+                </li>
+            </ul>
+        </button>
         
     </header>
   )
