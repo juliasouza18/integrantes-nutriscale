cfc9f68ab090003f926dd015913cf2f0f2da9da6
Julia Souza <julia.souza18@eaportal.org>
2024-08-12 21:31:23 -0300

    imagem consertada (?)

--- src/components/Integrantes.jsx
+++ src/components/Integrantes.jsx
@@ -8,33 +8,33 @@ export default function Integrantes() {
         <div className='flex flex-col items-center justify-center md:flex-row md:gap-16'>
           <section>
             <div className='w-80 md:w-96 p-8 h-auto  flex flex-col justify-normal items-center bg-fundoHeader left-10 rounded-[8px] mt-10 '>
-              <img className=' w-48 h-48 ' src="/src/assets/pedroca.svg" alt="" />
+              <img className=' w-48 h-48 ' src="/pedroca.svg" alt="" />
               <p className='font-bold text-2xl pt-6 font-sans'>Pedro H. Gomes</p>
               <p className='text-xl'><span className='font-bold'>Idade:</span>17 anos</p>
               <p className='text-xl'> <span className='font-bold'>Formação:</span> Unasp- Técnico de Informática</p>
-              <a className='pt-6 ' href="https://www.instagram.com/_phgomex?igsh=Z2puczV0bGFyamJu"> <img src="/src/assets/insta.svg" alt="" /></a>
+              <a className='pt-6 ' href="https://www.instagram.com/_phgomex?igsh=Z2puczV0bGFyamJu"> <img src="/insta.svg" alt="" /></a>
             </div>
 
           </section>
 
           <section className='py-20'>
             <div className='w-80 md:w-96 p-8 h-auto  flex flex-col justify-normal items-center bg-fundoHeader left-10 rounded-[8px] mt-10'>
-              <img className=' w-48 h-48 ' src="/src/assets/julia.svg" alt="" />
+              <img className=' w-48 h-48 ' src="/julia.svg" alt="" />
               <p className='font-bold text-2xl pt-6 font-sans'>Julia Schiavi</p>
               <p className='text-xl'><span className='font-bold'>Idade:</span>18 anos</p>
               <p className='text-xl'> <span className='font-bold'>Formação:</span> Unasp- Técnico de Informática</p>
-              <a className='pt-6 ' href="https://www.instagram.com/juh_schiavi18?igsh=ZnVzMHZ1dWc2am4x&utm_source=qr"> <img src="/src/assets/insta.svg" alt="" /></a>
+              <a className='pt-6 ' href="https://www.instagram.com/juh_schiavi18?igsh=ZnVzMHZ1dWc2am4x&utm_source=qr"> <img src="/insta.svg" alt="" /></a>
             </div>
 
           </section>
 
           <section>
             <div className='w-80 md:w-96 p-8 h-auto  flex flex-col justify-normal items-center bg-fundoHeader left-10 rounded-[8px] mt-10'>
-              <img className=' w-48 h-48 ' src="/src/assets/joao.svg" alt="" />
+              <img className=' w-48 h-48 ' src="/joao.svg" alt="" />
               <p className='font-bold text-2xl pt-6 font-sans'>João Victor Martins</p>
               <p className='text-xl'><span className='font-bold'>Idade:</span>17 anos</p>
               <p className='text-xl'> <span className='font-bold'>Formação:</span> Unasp- Técnico de Informática</p>
-              <a className='pt-6 ' href="https://www.instagram.com/jv.martins21?igsh=MW9hbmh0aDIzMHh2ZA=="> <img src="/src/assets/insta.svg" alt="" /></a>
+              <a className='pt-6 ' href="https://www.instagram.com/jv.martins21?igsh=MW9hbmh0aDIzMHh2ZA=="> <img src="/insta.svg" alt="" /></a>
             </div>
 
           </section>
